c11e1c52f043e082b6be3c2d0fd5e960e6a0a525
thirschler <hirschle@seattleu.edu>
2017-09-29 20:28:01 -0700

    hw1 commit

--- empdata.js
+++ empdata.js
@@ -1,124 +1,124 @@
 {
   "data": [
     {
-      "name": "Garrett Winters",
-      "designation": "Accountant",
-      "salary": "$170,750",
-      "joining_date": "2011/07/25",
-      "office": "Tokyo",
-      "extension": "8422"
+      "name": "Russell Wilson",
+      "designation": "QB",
+      "salary": "$18,800,000",
+      "joining_date": "2011",
+      "office": "Wisconsin",
+      "extension": "3"
     },
     {
-      "name": "Brielle Williamson",
-      "designation": "Integration Specialist",
-      "salary": "$372,000",
-      "joining_date": "2012/12/02",
-      "office": "New York",
-      "extension": "4804"
+      "name": "Doug Baldwin",
+      "designation": "WR",
+      "salary": "$4,418,750",
+      "joining_date": "2012",
+      "office": "Palo Alto",
+      "extension": "89"
     },
     {
-      "name": "Ashton Cox",
-      "designation": "Junior Technical Author",
-      "salary": "$86,000",
-      "joining_date": "2009/01/12",
-      "office": "San Francisco",
-      "extension": "1562"
+      "name": "Paul Richardson",
+      "designation": "WR",
+      "salary": "$1,499,538",
+      "joining_date": "2009",
+      "office": "Boulder",
+      "extension": "10"
     },
     {
-      "name": "Airi Satou",
-      "designation": "Accountant",
-      "salary": "$162,700",
-      "joining_date": "2008/11/28",
-      "office": "Tokyo",
-      "extension": "5407"
+      "name": "Tyler Lockett",
+      "designation": "WR",
+      "salary": "$891,687",
+      "joining_date": "2008",
+      "office": "Wichita",
+      "extension": "16"
     },
     {
-      "name": "Caesar Vance",
-      "designation": "Pre-Sales Support",
-      "salary": "$106,450",
-      "joining_date": "2011/12/12",
-      "office": "New York",
-      "extension": "8330"
+      "name": "Thomas Rawls",
+      "designation": "RB",
+      "salary": "$620,000",
+      "joining_date": "2011",
+      "office": "Champagne",
+      "extension": "34"
     },
     {
-      "name": "Shad Decker",
-      "designation": "Regional Director",
-      "salary": "$183,000",
-      "joining_date": "2008/11/13",
-      "office": "Edinburgh",
-      "extension": "6373"
+      "name": "Chris Carson",
+      "designation": "RB",
+      "salary": "$481,000",
+      "joining_date": "2008",
+      "office": "Cincinatti",
+      "extension": "34"
     },
     {
-      "name": "Cedric Kelly",
-      "designation": "Senior Javascript Developer",
-      "salary": "$433,060",
-      "joining_date": "2012/03/29",
-      "office": "Edinburgh",
-      "extension": "6224"
+      "name": "Jimmy Graham",
+      "designation": "TE",
+      "salary": "$10,000,000",
+      "joining_date": "2012",
+      "office": "New Orleans",
+      "extension": "88"
     },
     {
-      "name": "Haley Kennedy",
-      "designation": "Senior Marketing Designer",
-      "salary": "$313,500",
-      "joining_date": "2012/12/18",
+      "name": "Luke Willson",
+      "designation": "TE",
+      "salary": "$1,800,000",
+      "joining_date": "2012",
       "office": "London",
-      "extension": "3597"
+      "extension": "82"
     },
     {
-      "name": "Colleen Hurst",
-      "designation": "Javascript Developer",
-      "salary": "$205,500",
-      "joining_date": "2009/09/15",
+      "name": "Nick Vannett",
+      "designation": "TE",
+      "salary": "$726,220",
+      "joining_date": "2009",
+      "office": "Cleveland",
+      "extension": "86"
+    },
+    {
+      "name": "Ethan Pocic",
+      "designation": "G",
+      "salary": "$801,642",
+      "joining_date": "2012",
+      "office": "New Orleans",
+      "extension": "66"
+    },
+    {
+      "name": "Luke Joeckel",
+      "designation": "G",
+      "salary": "$7,250,000",
+      "joining_date": "2012",
       "office": "San Francisco",
-      "extension": "2360"
+      "extension": "76"
     },
     {
-      "name": "Dai Rios",
-      "designation": "Personnel Lead",
-      "salary": "$217,500",
-      "joining_date": "2012/09/26",
-      "office": "Edinburgh",
-      "extension": "2290"
+      "name": "Justin Britt",
+      "designation": "C",
+      "salary": "$2,349,933",
+      "joining_date": "2010",
+      "office": "Chicago",
+      "extension": "66"
     },
     {
-      "name": "Herrod Chandler",
-      "designation": "Sales Assistant",
-      "salary": "$137,500",
-      "joining_date": "2012/08/06",
-      "office": "San Francisco",
-      "extension": "9608"
+      "name": "Michael Bennett",
+      "designation": "DE",
+      "salary": "$11,268,750",
+      "joining_date": "2008",
+      "office": "Los Angeles",
+      "extension": "72"
     },
     {
-      "name": "Rhona Davidson",
-      "designation": "Integration Specialist",
-      "salary": "$327,900",
-      "joining_date": "2010/10/14",
-      "office": "Tokyo",
-      "extension": "6200"
+      "name": "Frank Clark",
+      "designation": "DT",
+      "salary": "$1,017,880",
+      "joining_date": "2008",
+      "office": "Ann Arbour",
+      "extension": "70"
     },
     {
-      "name": "Sonya Frost",
-      "designation": "Software Engineer",
-      "salary": "$103,600",
-      "joining_date": "2008/12/13",
-      "office": "Edinburgh",
-      "extension": "1667"
-    },
-    {
-      "name": "Jena Gaines",
-      "designation": "Office Manager",
-      "salary": "$90,560",
-      "joining_date": "2008/12/19",
-      "office": "London",
-      "extension": "3814"
-    },
-    {
-      "name": "Quinn Flynn",
-      "designation": "Support Lead",
-      "salary": "$342,000",
-      "joining_date": "2013/03/03",
-      "office": "Edinburgh",
-      "extension": "9497"
+      "name": "Earl Thomas",
+      "designation": "FS",
+      "salary": "$10,400,000",
+      "joining_date": "2013",
+      "office": "Austin",
+      "extension": "29"
     }
   ]
 }
